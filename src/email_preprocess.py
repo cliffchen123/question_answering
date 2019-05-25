2d96d8c7cf4a00cac8178301184fc357b806c13e
cliffchen123 <cliffchen123456@gmail.com>
2019-05-26 00:34:22 +0800

    finish simple data preprocess

--- src/email_preprocess.py
+++ src/email_preprocess.py
@@ -13,23 +13,27 @@ doc_file = 'WF Cases 20190517.csv'
 
 
 def find_description(input_str):
-	index = -1
+	
 	description_str = ['DESCRIPTION','Description','description']
 	for i in description_str:
 		if input_str.find(i)!=-1:
-			return index
-	return index
+			return input_str.find(i)
+	return -1
 
 
 test_rows = []
+isFirst_row = True
 with open(data_path+test_file) as csvfile:
 	rows = csv.reader(csvfile)
+
 	for row in rows:
-		new_row = [None,None]
+		if isFirst_row:
+			isFirst_row = False
+			continue
+		new_row = [None,None] # 0:description
 
 		''' row[3] is DESCRIPTION '''
 
-		
 		if row[3].count('\n') == 0 and len(row[3]) > 2: # only one line and word is enough
 			new_row[0] = row[3]
 		elif row[3].count('\n') != 0 and find_description(row[3])!=-1: # find the key word: 'description'
@@ -43,28 +47,54 @@ with open(data_path+test_file) as csvfile:
 				if row[3][i] == '\n' and description_main[-1] == '\n': # if appearing continuous '\n', then end 
 					break
 				description_main += row[3][i]
-			description_main.replace('-','')
+			description_main = description_main.replace('-','')
 			new_row[0] = description_main
 		else:
 			continue
 		# import pdb;pdb.set_trace()
 		test_rows.append(new_row)
+
+
 doc_rows = []
 with open(data_path+doc_file) as csvfile:
 	rows = csv.reader(csvfile)
 	new_rows = []
+	isFirst_row = True
 	for row in rows:
-		new_row = ''
+		if isFirst_row:
+			isFirst_row = False
+			continue		
+		if row[1]=='' or row[2]=='':
+			continue
+
+		new_row = ['',''] # 0:subject 1:description
 		isUseful = True
 
 		''' row[1] is Subject'''
 		for i in range(len(row[1])): 
+			''' remove tag [*] '''
 			if row[1][i]=='[':
 				isUseful = False
 			elif row[1][i]==']':
 				isUseful = True
 			elif isUseful:
-				new_row = row[1][i]
+				new_row[0] += row[1][i]
+
+		''' row[2] is Subject'''
+		for i in range(len(row[2])): 
+			''' remove tag [*] '''
+			if row[2][i]=='[':
+				isUseful = False
+			elif row[2][i]==']':
+				isUseful = True
+			elif isUseful:
+				new_row[1] += row[2][i]
+		try:
+			new_row[1] = new_row[1].replace('-','')
+		except:
+			import pdb;pdb.set_trace()
+
+
 		new_rows.append(new_row)
 doc_rows = new_rows
 import pdb;pdb.set_trace()
